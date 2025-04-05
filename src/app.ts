eeed36b36f8f2e6349bb11bedf944ab0ee734de1
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-04-05 10:18:45 -0600

    Funcionalidad con yargs

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,17 @@
 
+import { yarg } from './config/plugins/args.plugin';
 
+// console.log(process.argv);
+
+// const [tsnode, app, ...args] = process.argv;
+// console.log(args);
+
+// console.log(yarg);
+
+(async() => {
+    await main();
+})();
+
+async function main (){
+    console.log(yarg);
+}
